002e422ebaa2f42043dd5abe5070e4f6bceef17e
Andrew Martin <57409463+AMCodeBytes@users.noreply.github.com>
2025-03-14 15:34:06 +0000

    Created todo basics

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices  []string         // items on the list
+	cursor   int              // which item our cursor is pointing at
+	selected map[int]struct{} // which items are selected
+}
+
+func initialModel() model {
+	return model{
+		choices:  []string{"Practice coding in GO", "Improve my video editing", "Clean my desk"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "w":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "s":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	// The View function is also where you could use Lip Gloss to style the view
+	// Header
+	s := "What do I need to do?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// Footer
+	s += "\n Press q to quit.\n"
+
+	// Send UI to be rendered
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there has been an error: %v", err)
+		os.Exit(1)
+	}
+}
